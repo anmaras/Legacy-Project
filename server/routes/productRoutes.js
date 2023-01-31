326c387472a86449290c29ab1e69511e8e93eb96
Anthony Maras <marasantonis@gmail.com>
2023-01-31 19:09:01 +0200

    update

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,14 +1,21 @@
-import express from "express";
-import {createProduct, getAllProducts, getOneProduct, updateProduct, updateQuantity, removeProduct} from "../controllers/productControllers.js";
+import express from 'express';
+import {
+  createProduct,
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  updateQuantity,
+  removeProduct,
+} from '../controllers/productControllers.js';
 
 //Creating routes for products
 const router = express.Router();
 
-router.post("/", createProduct);
-router.get("/", getAllProducts);
-router.get("/:id", getOneProduct);
-router.put("/:id", updateProduct);
-router.put("/quantity/:id", updateQuantity);
-router.delete("/:id", removeProduct);
+router.post('/', createProduct);
+router.get('/', getAllProducts);
+router.get('/:id', getOneProduct);
+router.put('/:id', updateProduct);
+router.put('/quantity/:id', updateQuantity);
+router.delete('/:id', removeProduct);
 
 export default router;
